b445e5a5673f145d15e153f420fe95803b6bb85a
skywalker-alt327 <itisgoingtobeavictory2.39@gmail.com>
2023-05-25 23:28:47 -0400

    fixed a bug and enumrated all the ips with the mac addresses that responds to the arp request

--- network_scanner.py
+++ network_scanner.py
@@ -1,22 +1,24 @@
 import scapy.all as scapy
 
 def scan(ip):
-    # scapy.arping(ip)
+    #scapy.arping(ip)
     arp_request = scapy.ARP(pdst=ip)                                        # instance of an arp request packet
-    # print(scapy.ls(scapy.ARP()))
-    arp_request.show()
+    #print(scapy.ls(scapy.ARP()))
+    #arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")                        # instance of an ethernet packet
-    broadcast.show()
-    arp_request_broadcast = arp_request/broadcast                           # combines the two packets
-    arp_request_broadcast.show()
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]          # searches the MAC address of each device within the network
-                                                                            # and looks for the one that has the ip of the arp_request packet,
-                                                                            # we configured to search every MAC address by setting up the Ether dst to ff:ff:ff:ff:ff:ff
-                                                                            # answered are the answered ips
+    #broadcast.show()
+    arp_request_broadcast = broadcast/arp_request                           # combines the two packets
+    #arp_request_broadcast.show()
+
+    # searches the MAC address of each device within the network
+    # and looks for the one that has the ip of the arp_request packet,
+    # we configured to search every MAC address by setting up the Ether dst to ff:ff:ff:ff:ff:ff
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    print("IP" + "\t\t\t" + "MAC Address" + "\n-----------------------------------------")
     for answer in answered_list:
-        print(answer[1].psrc)                       # ip address of the client that responded to the arp request
-        print(answer[1].hwsrc)                      # mac address of the client with that ip that responded to the arp request
-        print("------------------------------------------------------------")
+        # ip address of the client that responded to the arp request
+        # mac address of the client with that ip that responded to the arp request
+        print(answer[1].psrc + "\t\t" + answer[1].hwsrc)
 
 def main():
     scan("192.168.173.2/24")
